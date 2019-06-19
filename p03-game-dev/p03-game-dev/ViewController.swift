b1ab0d59674034e8286f5e5217569c96a8baf656
Matthew Reid <merknicks11@gmail.com>
2019-06-18 23:56:54 -0400

    first commit, added UI for first screen

---
+++ p03-game-dev/p03-game-dev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  p03-game-dev
+//
+//  Created by Matthew Reid on 6/18/19.
+//  Copyright © 2019 Matthew Reid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
