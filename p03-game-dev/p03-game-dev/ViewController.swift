01781bf4565fbda66c27caa8dab23f686974057d
Matthew Reid <merknicks11@gmail.com>
2019-06-23 19:05:56 -0400

    first view done

--- p03-game-dev/p03-game-dev/ViewController.swift
+++ p03-game-dev/p03-game-dev/ViewController.swift
@@ -8,13 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var celsiusBtn: UIButton!
+    @IBOutlet weak var farenheitBtn: UIButton!
+    @IBOutlet weak var tempField: UITextField!
+    @IBOutlet weak var vacationBtn: UIButton!
+
+    
+    var winterColor : UIColor?
+    var summerColor : UIColor?
+    var vacation : Int = -1
+    var isFarenheit : Bool = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tempField.delegate = self
+        
+        summerColor = slider.minimumTrackTintColor
+        winterColor = slider.tintColor
     }
+    
+    @IBAction func farenheitSelected(_ sender: UIButton) {
+        self.isFarenheit = true
+        celsiusBtn.backgroundColor = self.view.backgroundColor
+        celsiusBtn.setTitleColor(UIColor.init(cgColor: celsiusBtn.layer.borderColor!), for: .normal)
+    
+    }
+    
+    @IBAction func celsiusSelected(_ sender: UIButton) {
+        self.isFarenheit = false
+        farenheitBtn.backgroundColor = self.view.backgroundColor
+        farenheitBtn.setTitleColor(UIColor.init(cgColor: farenheitBtn.layer.borderColor!), for: .normal)
+        
+        
+        celsiusBtn.backgroundColor = UIColor.init(cgColor: celsiusBtn.layer.borderColor!)
+        celsiusBtn.setTitleColor(UIColor.white, for: .normal)
+    }
+    
+    
 
+    @IBAction func valueChanged(_ sender: UISlider) {
+        if !isFarenheit {
+            let val = (slider.value - 32) * (5/9)
+            tempField.text = "\(val)"
+        }
+        if isFarenheit {
+            if slider.value < 60 {
+                slider.value = 0
+                slider.minimumTrackTintColor = winterColor
+                slider.thumbTintColor = winterColor
+             }
+        else {
+                slider.value = 1
+                slider.minimumTrackTintColor = summerColor
+                slider.thumbTintColor = summerColor
+            }
+        }
+    }
+    
 
 }
 
